5300111e06d917f3ba117da92e34221e57be0749
PatilManas3333 <patilmanas3333@gmail.com>
2025-04-08 23:58:07 +0530

    tenth day

---
+++ strings.py
@@ -0,0 +1,21 @@
+str1 = "Hello everyone, I am Manas.\n"
+str2 = "I am a \"clever\" student.\n"
+str3 = 'I am a "clever" student.\n'
+print(str1,str2,str3)
+str4 = '''Hii everyone,
+I am Manas Patil,
+persueing my btech degree in computer engineering.'''
+print(str4)
+str = "manas"
+print(str[0])
+print(str[4])
+print("Let's use for loop:")
+for ch in str :
+    print(ch)
+print(str[0:])
+print(str[0:3])
+
+# Quick Quiz :
+
+name = "Harry"
+print(name[-4:-2])
