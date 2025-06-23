75f65bfdb49cd874b492090226c424298585a756
PatilManas3333 <patilmanas3333@gmail.com>
2025-06-23 20:45:48 +0530

    new commit

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# tuple is immutable
+
+tuple = ("manas", "kiran", "patil", "manas", "mansi")
+print(tuple)
+
+# 1. count() : count the total occurance of a element.
+res = tuple.count("manas")
+print(res)
